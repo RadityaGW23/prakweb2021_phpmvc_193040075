2caa6e080ba2ef484772120731c557f3cc9e89ce
Raditya Gilang Wicaksono <rgilang30@gmail.com>
2021-10-01 19:25:50 +0700

    Tugas 4, menyelesaikan video ke 5 tentang View

--- phpnvc/app/controllers/About.php
+++ phpnvc/app/controllers/About.php
@@ -1,12 +1,21 @@
 <?php
 
-class About {
+class About extends Controller{
     public function index($nama = "Mark", $pekerjaan = "CEO")
     {
-        echo "Hallo nama saya $nama, dan saya seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
+
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
